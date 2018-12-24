98a170074f0586ae5f582011267a93671c1bcc97
soga0901 <k.sogafish@gmail.com>
2018-12-24 17:01:44 +0900

    :sparkles: Add Tasks Form

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,10 +1,32 @@
 import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks';
+
 import './body.html';
 
 Template.body.helpers({
-  tasks: [
-    { text: 'TEXT1____________________________' },
-    { text: 'TEXT2____________________________' },
-    { text: 'TEXT3____________________________' },
-  ],
+  tasks() {
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  }
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    if (!text.length) {
+      window.alert('Type Something')
+
+      return false;
+    }
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    target.text.value = '';
+  }
 });
